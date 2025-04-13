95271295d906511acab0cb72fafa4d53b8d77333
orrodguez19 <miguelorlandos97@gmail.com>
2025-04-12 23:42:57 -0400

    Crear install_deltachat.sh

---
+++ install_deltachat.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Descarga el binario de deltachat CLI para Linux x86_64
+wget https://github.com/deltachat/deltachat-core-rust/releases/latest/download/deltachat-cli-linux-x86_64 -O /usr/local/bin/deltachat
+# Dale permisos de ejecución
+chmod +x /usr/local/bin/deltachat
